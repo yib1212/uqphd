8e15a727e36871f373297fedb40e2a3ed958031f
SnowingRamble <48935561+yib1212@users.noreply.github.com>
2022-07-21 23:55:23 +1000

    Add files via upload

---
+++ objective 1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 21 03:35:31 2022
+
+@author: Yibo Wang
+"""
+
+import pyodbc
+import pandas as pd
+import numpy as np
+
+conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+            r'DBQ=data\Travel Survey\2018-21_pooled_seq_qts_erv1.0.accdb;')
+
+conn = pyodbc.connect(conn_str)
+
+cursor = conn.cursor()
+
+for i in cursor.tables(tableType='TABLE'):
+    print(i.table_name)
+
+df = pd.read_sql('select * from 5_QTS_TRIPS', conn)
+
+duration = np.array(df)[:, 23]
+cumdist = np.array(df)[:, 24]
+
+print(df)
+

eadf95d0e85a5dce68c34256de4d00b932e667bd
yib1212 <wangyibo19951212@163.com>
2022-07-22 10:24:07 +1000

    0722

--- objective 1.py
+++ objective 1.py
@@ -24,34 +24,79 @@ def ReadDatabase():
     cursor = conn.cursor()
     for i in cursor.tables(tableType='TABLE'):
         print(i.table_name)
+        
+    return conn
+
+
+def ReadTable(conn, table_name):
     
     # Table name
-    df = pd.read_sql('select * from 5_QTS_TRIPS', conn)
+    df = pd.read_sql(f'select * from {table_name}', conn)
     
     return df
 
 
 def ModeChoice(data_frame):
     
+    ''' Compute the proportion of transport mode share. '''
+    
     mainmode = np.array(data_frame)[:, 12]
     
-    
-    
+    total = np.size(mainmode)
     data_count = collections.Counter(mainmode)
     
-    print(data_count)
+    num_pri = data_count['Car driver'] \
+            + data_count['Car passenger'] \
+            + data_count['Truck driver'] \
+            + data_count['Motorcycle driver'] \
+            + data_count['Motorcycle passenger']
+    num_act = data_count['Walking'] \
+            + data_count['Bicycle']
+    num_pub = data_count['Train'] \
+            + data_count['Ferry'] \
+            + data_count['Light rail'] \
+            + data_count['Mobility scooter'] \
+            + data_count['Public bus'] \
+            + data_count['Public Bus'] \
+            + data_count['School bus (with route number)'] \
+            + data_count['School bus (private/chartered)'] \
+            + data_count['Charter/Courtesy/Other bus'] \
+            + data_count['Other method']
+    num_shr = data_count['Taxi'] \
+            + data_count['Uber / Other Ride Share']
+    
+    prop_pri = num_pri / total * 100
+    prop_act = num_act / total * 100
+    prop_pub = num_pub / total * 100
+    prop_shr = num_shr / total * 100
+            
+    print("Mode share of pirvate vehicle: %.2f%%"   % prop_pri)
+    print("Mode share of ativate transport: %.2f%%" % prop_act)
+    print("Mode share of public transport: %.2f%%"  % prop_pub)
+    print("Mode share of ride share: %.2f%%"        % prop_shr)
     
     
-def TimePerKilo(data_frame):
+def TimePerKilo(df1, df2):
     
-    duration = np.array(data_frame)[:, 23]
-    cumdist = np.array(data_frame)[:, 24]
+    # Counter({'petrol': 20407, 'diesel': 5764, 'hybrid': 154, 'lpg': 79, 'electric': 34, None: 31})
+    fueltype = np.array(df1)[:, 2]
+    # Counter({'car': 22076, 'van': 3470, 'motorcycle': 770, 'truck': 115, 'other': 38})
+    vehitype = np.array(df1)[:, 3]
+    duration = np.array(df2)[:, 23]
+    cumdist = np.array(df2)[:, 24]
+    
+    fuel_count = collections.Counter(fueltype)
+    vehi_count = collections.Counter(vehitype)
     
     time_per_kilo = np.divide(duration, cumdist, out=np.zeros_like(duration), where=cumdist!=0)
+    print(fuel_count)
+    print(vehi_count)
     
 
 if __name__ == "__main__":
     
-    df = ReadDatabase()
-    ModeChoice(df)
-    
+    conn = ReadDatabase()
+    df_3 = ReadTable(conn, '3_QTS_VEHICLES')
+    df_5 = ReadTable(conn, '5_QTS_TRIPS')
+    TimePerKilo(df_3, df_5)
+    #ModeChoice(df_5)

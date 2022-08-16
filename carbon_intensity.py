32caedf6a427a7b5720bc4150a87b829058359d1
yib1212 <wangyibo19951212@163.com>
2022-08-16 23:07:21 +1000

    0816

--- carbon_intensity.py
+++ carbon_intensity.py
@@ -8,3 +8,22 @@ Created on Mon Aug 15 17:08:03 2022
 import numpy as np
 import pandas as pd
 import collections
+
+
+class CarbonIntensity(object):
+    
+    def __init__(self):
+        
+        # Unit: MtCO2
+        self.cars = 10.213
+        self.trucks_buses = 4.908
+        self.motorcycles = 0.068
+        self.railways = 0.566
+        
+        # Unit: g/km
+        self.petrol = 165
+        self.diesel = 213
+        
+        # Unit： g/pkm
+        self.car_2018 = 154.5
+        self.bus_2018 = 79

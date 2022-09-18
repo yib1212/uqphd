0f01d95f0d6b28f1dac5042e53b98e3c30d07bf3
yib1212 <wangyibo19951212@163.com>
2022-09-18 21:12:22 +1000

    0918

--- levy_2019.py
+++ levy_2019.py
@@ -4,21 +4,21 @@ Created on Wed Aug 31 13:48:24 2022
 
 @author: Yibo Wang
 """
-import csv
+
 import math
 import pyodbc
+import collections
 import numpy as np
 import pandas as pd
 from objective_1 import CarbonEmission
 from scipy.stats import levy
-from scipy.stats import norm
-from scipy.stats import skewnorm
-from sklearn import preprocessing
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import scipy.special as sp
 from sklearn.model_selection import train_test_split
+from scipy.stats import chi2
 from scipy.ndimage import gaussian_filter
+
 
 class LevyFitting(object):
     
@@ -33,16 +33,15 @@ class LevyFitting(object):
         self.bus_2019 = 97.03
         self.bus_2020 = 121.97
         
+        self.car_ave = 152.3
+        self.bus_ave = 100.2
+        
         # Database location
         conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-                    r'DBQ=data\Travel Survey\2019-20.accdb;')
+                    r'DBQ=data\Travel Survey\2019.accdb;')
         
         conn = pyodbc.connect(conn_str)
-        
-        cursor = conn.cursor()
-        for i in cursor.tables(tableType='TABLE'):
-            print(i.table_name)
-            
+                    
         self.df_1 = pd.read_sql(f'select * from 1_QTS_HOUSEHOLDS', conn)
         self.df_3 = pd.read_sql(f'select * from 3_QTS_VEHICLES', conn)
         self.df_5 = pd.read_sql(f'select * from 5_QTS_TRIPS', conn)
@@ -53,7 +52,45 @@ class LevyFitting(object):
         
         return None
     
-
+    
+    def SkewNorm(self, x, sigma, mu, alpha, c, a):
+        
+        normpdf = (1/(sigma*np.sqrt(2*math.pi)))*np.exp(-(np.power((x-mu),2)/(2*np.power(sigma,2))))
+        normcdf = (0.5*(1+sp.erf((alpha*((x-mu)/sigma))/(np.sqrt(2)))))
+        
+        return 2 * a * normpdf * normcdf + c
+    
+    
+    def Norm(self, x, sigma, mu, alpha, c, a):
+        
+        normpdf = (1/(sigma*np.sqrt(2*math.pi)))*np.exp(-(np.power((x-mu),2)/(2*np.power(sigma,2))))
+        
+        return a * normpdf + c
+    
+    
+    def Levy(self, x, sigma, mu, a, c):
+        
+        # levy_fit = np.sqrt(sigma/(2*math.pi)) * (np.exp(-(sigma/2*(x-mu)))) / (np.power((x-mu),2))
+        levy_fit = levy.pdf(x, mu, sigma)
+        
+        return a * levy_fit
+    
+    
+    def GaussianKernal(self, x):
+        
+        sigma = 2500
+        mean = 1700
+        e = math.e
+        pi = math.pi
+        
+        y = []
+        for i in x:
+            result = ((1/(sigma*(math.sqrt(2*pi)))) * (e**(-0.5*((x[i]-mean)/sigma)**2)))
+            y.append(result)
+        
+        return y
+    
+    
     def TripEmission(self):
         
         ''' Compute the carbon emission for each trip. '''
@@ -64,7 +101,11 @@ class LevyFitting(object):
         car_list = []
         bus_list = []
         zero_cnt = 0
-        
+        max_cnt = 0
+        min_bound = 0
+        max_bound = 10000
+        d = 10
+                
         for i in range(len(mode_id)):
             if mode_id[i] == 0 or mode_id[i] == 2:
                 carbon_emi.append(cum_dist[i] * self.car_2019)
@@ -76,194 +117,252 @@ class LevyFitting(object):
                 carbon_emi.append(0)
                 zero_cnt += 1
         
-        non_zero = len(mode_id) - zero_cnt
-        #carbon_emi = np.round(np.array(carbon_emi), 2)
-        
         hist_emi = carbon_emi
         
         for index, value in enumerate(hist_emi):
-            if value > 15000:
-                hist_emi[index] = 15000
+            if value > max_bound:
+                max_cnt += 1
+                hist_emi[index] = max_bound
+        
+        self.non_zero = len(mode_id) - zero_cnt - max_bound
                 
-        d = 10
-        min_bound = int(min(hist_emi))
-        max_bound = int(max(hist_emi))
         num_bins = (max_bound - min_bound) // d
-                
-        ''' Levy distribution '''
-        x = range(15000)
-        y = car_list + bus_list
-        para = levy.fit(y)
         
-        gau = skewnorm.pdf(x, a=7, loc=300, scale=4000)
-
-        
-        # for i in range(len(gau)):
-        #     p[i] *= gau[i]
-        # sum_p = sum(p)
-        # for i in range(len(gau)):
-        #     p[i] = p[i] / sum_p * 0.8428814301079163
         self.hist_emi = hist_emi
         self.num_bins = num_bins
         
-        # plt.axis([0, 15000, 0, 800])
+        plt.axis([0, max_bound, 0, 250])
         self.n, bin_edges, _ = plt.hist(hist_emi, num_bins)
-        self.p = levy.pdf(x, *para) * sum(self.n) * d
-        
-        # plt.plot(x, 933080*p, 'k', linewidth=2, c='red')
-        # plt.title('Carbon emission of each trip by motor vehicle', self.font)
-        # plt.xlabel('Carbon emissions (g)', self.font)
-        # plt.ylabel('Number of trips', self.font)
-        # plt.show()
-        
-        ''' Plot the skew normal weight '''
-        # gau = skewnorm.pdf(x, a=8, loc=120, scale=3200)
-        # plt.axis([0, 15000, 0, 3.5e-4])
-        # plt.plot(x, gau, 'k', linewidth=2, c='red')
-        # plt.show()
-        
-        ''' Scatter the weight point '''
-        weight = []
-        bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-        estimate = levy.pdf(bin_middles, *para)
         self.n[0] = 0
         self.n[-1] = 0
-        for i in range(len(self.n)):
-            weight.append(self.n[i] / (estimate[i] * non_zero))
-        
-        weight = preprocessing.normalize([weight])[0]
-        
-        plt.axis([0, 15000, 0, 0.06])
-        plt.scatter(bin_middles, weight, s=10, c='red')
+        self.bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+        plt.title('Carbon emission of each trip by motor vehicle', self.font)
+        plt.xlabel('Carbon emissions (g)', self.font)
+        plt.ylabel('Number of trips', self.font)
         plt.show()
+                        
+        return self.n, self.non_zero, self.hist_emi
         
-        return bin_middles, weight, para
-    
-    
-    def SkewNorm(self, x, sigma, mu, alpha, c, a):
         
-        normpdf = (1/(sigma*np.sqrt(2*math.pi)))*np.exp(-(np.power((x-mu),2)/(2*np.power(sigma,2))))
-        normcdf = (0.5*(1+sp.erf((alpha*((x-mu)/sigma))/(np.sqrt(2)))))
+    def LevyFitting(self, weight, non_zero, emission):
         
-        return 2 * a * normpdf * normcdf + c
-    
-    
-    def WeightFitting(self, bin_middles, weight):
+        ''' Levy Fitting '''
+        bin_middles = self.bin_middles
+        weight = np.array(weight) / sum(weight)
         
-        for i in range(10):
-            weight = gaussian_filter(weight, sigma=1)
-        
-        X_train, X_test, y_train, y_test = train_test_split(bin_middles, weight, test_size=0.2, random_state=0)
-        popt, pcov = curve_fit(self.SkewNorm, X_train, y_train, p0=(4000, 300 ,7,0,0))
+        popt, pcov = curve_fit(self.Levy, bin_middles, weight, p0=( 600, 300, 100, 0))
         print(popt)
         
-        y_train_pred = self.SkewNorm(X_train,*popt)
+        y_train_pred = self.Levy(bin_middles, *popt)
         
-        plt.axis([0, 10000, 0, 0.06])
-        plt.scatter(X_train, y_train, s=5, c='blue', label='train')
-        plt.scatter(X_train, y_train_pred, s=5, c='red', label='model')
+        plt.axis([0, 10000, 0, 0.01])
+        plt.scatter(bin_middles, weight, s=5, c='blue', label='train')
+        plt.scatter(bin_middles, y_train_pred, s=5, c='red', label='model')
         plt.grid()
         plt.legend()
         plt.xlabel('Carbon emissions (g)', self.font)
         plt.ylabel('Weight', self.font)
         plt.show()
         
-        return popt
-    
-    
-    def SkewNormalLevy(self, para, popt):
+        ''' Plot the Levy curve '''
+        x = range(10000)
+        y = self.Levy(x, *popt)
+        y_all = self.Levy(bin_middles, *popt)
+        y_all[y_all < 0] = 0
+        y = y / sum(y_all) * non_zero
+        # y_train_pred = y_train_pred / sum(y_train_pred) * self.non_zero
         
-        print(para)
-        x = range(15000)
-        y_levy = levy.pdf(x, *para)
-        y_skew = self.SkewNorm(x,*popt)
-        y = []
-        
-        for i in range(15000):
-            y.append(y_levy[i] * y_skew[i])
-        sum_skew = sum(y_skew)
-        sum_levy = sum(y_levy)
-        sum_n = sum(self.n)
-        
-        for i in range(15000):
-            y[i] = y[i] * sum_skew / sum_levy * sum_n
-        
-        
-        plt.axis([0, 10000, 0, 320])
-        plt.hist(self.hist_emi, self.num_bins, color='blue')
-        plt.plot(x, y, 'k', linewidth=2, c='red', label='Skew-Levy')
-        plt.plot(x, self.p, 'k', linewidth=2, c='black', label='Levy')
-        plt.legend()
-        # plt.title('Carbon emission of each trip by motor vehicle', self.font)
+        plt.axis([0, 10000, 0, 250])
+        plt.plot(x, y, 'k', linewidth=2, c='red', label='Levy')
+        plt.hist(emission, self.num_bins, color='blue')
         plt.xlabel('Carbon emissions (g)', self.font)
         plt.ylabel('Number of trips', self.font)
         plt.show()
         
-        return 
+        return popt
     
     
-    def ProfileLikelihood(self, bin_middles, weight):
+    def Weight(self, popt_levy):
         
-        ''' Fitting skew normal distribution '''
-        weight = (np.array(weight) * 10).astype(int)
-        weight[0] = 0
+        bin_middles = self.bin_middles
+        n = np.array(self.n)
+        non_zero = self.non_zero
+        estimate = np.array(self.Levy(bin_middles, *popt_levy))
+        
+        ''' Compute the Weight '''
+        weight = n / (estimate * non_zero)
+        weight /= sum(weight)
         weight[-1] = 0
-        data = []
+        weight[0] = 0.0002
+        weight[weight > 0.0002] = 0.0002
         
-        for i in range(len(bin_middles)):
-            for j in range(weight[i]):
-                data.append(bin_middles[i])
-               
-        para_sn = skewnorm.fit(weight, f0=10, floc=120)
-        f_skew = skewnorm.pdf(bin_middles, *para_sn)
         
-        print(para_sn)
-        print(f_skew)
         
-        plt.axis([0, 15000, 0, 2e-4])
-        plt.plot(bin_middles, f_skew, 'k', linewidth=2, c='red')
+        ''' Gaussian filter '''
+        for i in range(20):
+            weight = gaussian_filter(weight, sigma=1)
+        
+        ''' Fit and predict '''
+        popt_norm, pcov = curve_fit(self.Norm, bin_middles, weight, p0=(400, 100 ,7,0,0))
+        print(popt_norm)
+        y_train_pred = self.Norm(bin_middles,*popt_norm)
+        
+        ''' Scatter the Weight and plot the prediction '''
+        plt.axis([0, 10000, 0, 0.0002])
+        plt.scatter(bin_middles, weight, s=5, c='blue', label='train')
+        plt.scatter(bin_middles, y_train_pred, s=5, c='red', label='model')
+        plt.grid()
+        plt.legend()
         plt.show()
         
-        # print(data)
-        # print(len(data))
-        # print(weight)
+        return popt_norm
+    
+    
+    def ChiSquare(self, popt_levy, popt_norm):
         
+        non_zero = self.non_zero
+        bin_middles = self.bin_middles
+        hist_emi =self.hist_emi
+                
+        ''' Plot the Levy and Skew-levy curve '''
+        x = range(10000)
+        y_levy = np.array(self.Levy(x, *popt_levy))
+        y_norm = np.array(self.Norm(x, *popt_norm))
+        y = y_levy * y_norm
+        y_levy = y_levy / np.sum(y_levy) * non_zero * 10
+        y = y / np.sum(y) * np.sum(y_levy)
+                
+        plt.axis([0, 10000, 0, 250])
+        plt.hist(hist_emi, self.num_bins, color='blue')
+        plt.plot(x, y, 'k', linewidth=2, c='red', label='Skew-Levy')
+        plt.plot(x, y_levy, 'k', linewidth=2, c='black', label='Levy')
+        plt.legend()
+        plt.xlabel('Carbon emissions (g)', self.font)
+        plt.ylabel('Number of trips', self.font)
+        plt.show()
+        
+        ''' Compute the Chi-square '''
+        A = np.array(self.n)
+        E_levy = np.array(self.Levy(bin_middles, *popt_levy))
+        E_norm = np.array(self.Norm(bin_middles, *popt_norm))
+        E = E_levy * E_norm
+        E = E / np.sum(E) * non_zero
+        div = np.divide(np.square(A - E), E, out=np.zeros_like(E), where=E!=0)
+        X_2 = np.sum(div)
+        
+        print(X_2)
+        
+        return E_norm    
+    
+    
+    def TravelPurpose(self, ):
+        
+        carbon_emi = self.hist_emi
+                        
+        purpose = np.array(self.df_5)[:, 25]
+        print(collections.Counter(purpose))
+        
+        commute = []
+        shopping = []
+        pickup = []
+        recreation = []
+        education = []
+        business = []
+        accompany = []
+        work = []
+        social = []
+        deliver = []
+        other = []
+        
+        for index, value in enumerate(purpose):
+            if value == 'Direct Work Commute':
+                commute.append(carbon_emi[index])
+            elif value == 'Shopping':
+                shopping.append(carbon_emi[index])
+            elif value == 'Pickup/Dropoff Someone':
+                pickup.append(carbon_emi[index])
+            elif value == 'Recreation':
+                recreation.append(carbon_emi[index])
+            elif value == 'Education':
+                education.append(carbon_emi[index])
+            elif value == 'Personal Business':
+                business.append(carbon_emi[index])
+            elif value == 'Accompany Someone':
+                accompany.append(carbon_emi[index])
+            elif value == 'Work Related':
+                work.append(carbon_emi[index])
+            elif value == 'Social':
+                social.append(carbon_emi[index])
+            elif value == 'Pickup/Deliver Something':
+                deliver.append(carbon_emi[index])
+            else:
+                other.append(carbon_emi[index])
+        
+        dict_purp = {'commute':    commute,
+                     'shopping':   shopping,
+                     'pickup':     pickup,
+                     'recreation': recreation,
+                     'education':  education, 
+                     'business':   business,
+                     'work':       work
+                     }
+        
+        return dict_purp
+    
+    
+    def PurposeAnalysis(self, dict_purp):
+        
+        num_bins = self.num_bins
+        dist_estm = {}
+                
+        for key in dict_purp:
+            print(key)
+        
+            plt.axis([0, 10000, 0, 250])
+            n, _, _ = plt.hist(dict_purp[key], num_bins)
+            plt.show()
+        
+            non_zero = len(dict_purp[key]) - n[0] - n[-1]
+            n[0] = 0
+            n[-1] = 0
+            
+            dist_estm[key] = self.LevyFitting(n, non_zero, dict_purp[key])
+        
+        x = range(10000)
+        plt.axis([0, 10000, 0, 200])
+        plt.plot(x, dist_estm['commute'], 'k', linewidth=2, c='red', label='Commute')
+        plt.plot(x, dist_estm['shopping'], 'k', linewidth=2, c='blue', label='Shopping')
+        plt.plot(x, dist_estm['pickup'], 'k', linewidth=2, c='green', label='Pickup')
+        plt.plot(x, dist_estm['recreation'], 'k', linewidth=2, c='yellow', label='Recreation')
+        plt.plot(x, dist_estm['education'], 'k', linewidth=2, c='black', label='Education')
+        plt.plot(x, dist_estm['business'], 'k', linewidth=2, c='orange', label='Personal business')
+        plt.plot(x, dist_estm['work'], 'k', linewidth=2, c='purple', label='Work related')
+        plt.legend()
+        plt.xlabel('Carbon emissions (g)', self.font)
+        plt.ylabel('Number of trips', self.font)
+        plt.show()
         
         return None
-    
-    
-    def GaussianKernal(self, x):
         
-        sigma = 2500
-        mean = 1700
-        e = math.e
-        pi = math.pi
-        
-        # gaussian = (1/(sigma*(math.sqrt(2*pi)))) * (e^(-0.5*((x-mean)/sigma)^2))
-        
-        y = []
-        for i in x:
-            result = ((1/(sigma*(math.sqrt(2*pi)))) * (e**(-0.5*((x[i]-mean)/sigma)**2)))
-            y.append(result)
-        
-        return y
-    
-
     
     
-
+    
 if __name__ == "__main__":
     
     levy_fitting = LevyFitting()
-    bin_middles, weight, para = levy_fitting.TripEmission()
-    # levy_fitting.ProfileLikelihood(bin_middles, weight)
-    popt = levy_fitting.WeightFitting(bin_middles, weight)
-    levy_fitting.SkewNormalLevy(para, popt)
+    weight, non_zero, emission = levy_fitting.TripEmission()
     
-    # with open('test.csv', 'w') as f:
-    #     # create the csv writer
-    #     writer = csv.writer(f)
-    #     # write a row to the csv file
-    #     writer.writerow(weight)
-    #     writer.writerow(bin_middles)
+    popt_levy = levy_fitting.LevyFitting(weight, non_zero, emission)
+    popt_norm = levy_fitting.Weight(popt_levy)
+    y_norm = levy_fitting.ChiSquare(popt_levy, popt_norm)
     
+    print(chi2.ppf(0.5,1000))
+    
+    # for i in range(10):
+    #     weight /= y_norm
+    #     popt_levy = levy_fitting.LevyFitting(weight, non_zero, emission)
+    #     popt_norm = levy_fitting.Weight(popt_levy)
+    #     y_norm = levy_fitting.ChiSquare(popt_levy, popt_norm)
+    
+    # dict_purp = levy_fitting.TravelPurpose()
+    # levy_fitting.PurposeAnalysis(dict_purp)

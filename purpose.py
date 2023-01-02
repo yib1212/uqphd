d08ddbcf3953f7d46d3d6977f0041ff4748e8f46
yib1212 <wangyibo19951212@163.com>
2023-01-02 16:27:17 +1000

    0102

--- purpose.py
+++ purpose.py
@@ -40,7 +40,7 @@ class LevyFitting(object):
         
         # Database location
         conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-                    r'DBQ=data\Travel Survey\2020.accdb;')
+                    r'DBQ=data\Travel Survey\2019.accdb;')
         
         conn = pyodbc.connect(conn_str)
                     
@@ -110,11 +110,11 @@ class LevyFitting(object):
                 
         for i in range(len(mode_id)):
             if mode_id[i] == 0 or mode_id[i] == 2:
-                carbon_emi.append(cum_dist[i] * self.car_2020)
-                car_list.append(cum_dist[i] * self.car_2020)
+                carbon_emi.append(cum_dist[i] * self.car_2019)
+                car_list.append(cum_dist[i] * self.car_2019)
             elif mode_id[i] == 3:
-                carbon_emi.append(cum_dist[i] * self.bus_2020)
-                bus_list.append(cum_dist[i] * self.bus_2020)
+                carbon_emi.append(cum_dist[i] * self.bus_2019)
+                bus_list.append(cum_dist[i] * self.bus_2019)
             else:
                 carbon_emi.append(0)
                 zero_cnt += 1
@@ -296,11 +296,11 @@ class LevyFitting(object):
         #           }
         
         w_max = {'commute':    0.0020,
-                  'shopping':   0.0003,
-                  'pickup':     0.001,
-                  'recreation': 0.0001,
-                  'education':  0.0025, 
-                  'business':   0.0010,
+                  'shopping':   0.0014,
+                  'pickup':     0.00012,
+                  'recreation': 0.0010,
+                  'education':  0.0007, 
+                  'business':   0.0011,
                   'work':       0.0011
                   }
 
@@ -316,7 +316,7 @@ class LevyFitting(object):
         p0 = {'commute':    (2726, 2738, 0, 0, 8),
               'shopping':   (3683, -2790, 0, 0, 10),
               'pickup':     (3200, -500, 0, 0, 5),
-              'recreation': (3042, -320, 0, 0, 9e-5),
+              'recreation': (2797, 243, 0, 0, 9e-5),
               'education':  (2000, 500, 0, 0, 5), 
               'business':   (4000, -1000, 0, 0, 15),
               'work':       (3042, 1400, 0, 0, 6)
@@ -388,7 +388,7 @@ class LevyFitting(object):
         plt.ylabel('Number of trips (10^)', self.font)
         plt.show()
         
-        return np.sum(dist_estm['shopping'])
+        return np.sum(dist_estm['recreation'])
     
     
     def polygon_under_graph(self, x, y):
@@ -529,7 +529,69 @@ class LevyFitting(object):
         return None
             
        
+    def RecreationResults(self):
         
+        y_levy = np.zeros((4, 1000))
+        y_norm = np.zeros((4, 1000))
+        y = np.zeros((4, 1000))
+        x = self.bin_middles
+        
+        popt_levy = [[903.86732213, 7.18356327, 15.05298344, 0.],
+                     [908.39271423, 26.39063912, 15.29474426, 0.],
+                     [861.76833284, 6.87323499, 15.0391782, 0.],
+                     [694.19437525, 53.20996437, 14.27691785, 0.]]
+        popt_norm = [[2394.62587, 418.131815, 0.00000000e+00, 0.0000343171486, 1.25236704],
+                     [2797.63614, 243.439671, 0.00000000e+00, 0.0000118294796, 1.31524791],
+                     [1998.27889, 821.056174, 0.00000000e+00, 0.0000631647039, 1.94677144],
+                     [4085.9307, -786.419002, 0.00000000e+00, -0.000000406283983, 0.470568787]]
+        sum_y = [29460.0, 28080.0, 20000.0, 21650.0]
+        
+        for i in range(4):
+            popt_l = popt_levy[i]
+            popt_n = popt_norm[i]
+            y_levy[i] = np.array(self.Levy(x, *popt_l))
+            y_norm[i] = np.array(self.Norm(x, *popt_n))
+            y[i] = y_levy[i] * y_norm[i]
+            y[i] /= np.sum(y[i]) 
+            y[i] = y[i] * sum_y[i] / 10
+            
+        plt.axis([0, 6000, 0, 0.015])
+
+        plt.plot(x, y[0], 'k', linewidth=2, color=(0.0, 0, 1.0), label='2017-2018')
+        plt.plot(x, y[1], 'k', linewidth=2, color=(0.3, 0, 0.6), label='2018-2019')
+        plt.plot(x, y[2], 'k', linewidth=2, color=(0.6, 0, 0.3), label='2019-2020')
+        plt.plot(x, y[3], 'k', linewidth=2, color=(1.0, 0, 0.0), label='2020-2021')
+        plt.legend()
+        plt.xlabel('Carbon emissions (g)', self.font)
+        plt.ylabel('PDF', self.font)
+        plt.show()
+        
+        
+        ax = plt.figure(figsize=(10,6)).add_subplot(projection='3d')
+        
+        years = range(1, 5)
+        
+        # verts[i] is a list of (x, y) pairs defining polygon i.
+        gamma = np.vectorize(math.gamma)
+        verts = [self.polygon_under_graph(x, y[i-1])
+                  for i in years]
+        
+        viridis = cm.get_cmap('plasma', 12)
+        facecolors = viridis.colors
+        facecolors = [[0.050383, 0.029803, 0.527975, 1.      ], [0.241396, 0.014979, 0.610259, 1.      ], [0.387183, 0.001434, 0.654177, 1.      ], [0.523633, 0.024532, 0.652901, 1.      ]]
+        facecolors = [[1, 1, 0, 1], [0.9, 0.9, 0, 1], [0.8, 0.8, 0, 1], [0.7, 0.7, 0, 1]]
+        print(facecolors)
+        
+        poly = PolyCollection(verts, facecolors=facecolors, alpha=.7)
+        ax.add_collection3d(poly, zs=years, zdir='y')
+        
+        ax.set(xlim=(0, 10000), ylim=(1, 4.5), zlim=(0, 30),
+               
+                xlabel='Carbon emissions (g)', ylabel=r'$year$', zlabel='Number of trips')
+        
+        plt.show()
+        
+        return None
         
     
     
@@ -547,4 +609,5 @@ if __name__ == "__main__":
     
     levy_fitting.CommuteResults()
     levy_fitting.ShoppingResults()
+    levy_fitting.RecreationResults()
 

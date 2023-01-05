bfaab5d7795765997e2ce7ca0e9c4eeed185d7ae
yib1212 <wangyibo19951212@163.com>
2023-01-05 13:31:50 +1000

    0105

---
+++ objective_2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 13:24:05 2023
+
+@author: Yibo Wang
+"""
+
+import pyodbc
+import dbfread
+import collections
+import pandas as pd
+import numpy as np
+
+
+class LevyRegression(object):
+    
+    def __init__(self):
+        
+        return None
+    
+    
+if __name__ == "__main__":
+    
+    reg = LevyRegression()
